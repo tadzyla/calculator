4d88785559081c8aff47b96edef0006a226b0415
tadzyla <tadas.zvinklys@gmail.com>
2022-12-06 21:30:20 +0000

    adding style, adding more logic

--- index.js
+++ index.js
@@ -1,28 +1,45 @@
 
 let userNumber = document.getElementById("input")
 let outputResult = document.getElementById("output")
-let oneButton = document.getElementById("one").textContent
-let twoButton = document.getElementById("two").textContent
-let threeButton = document.getElementById("three").textContent
-let fourButton = document.getElementById("four").textContent
-let fiveButton = document.getElementById("five").textContent
-let sixButton = document.getElementById("six").textContent
-let sevenButton = document.getElementById("seven").textContent
-let eightButton = document.getElementById("eight").textContent
-let nineButton = document.getElementById("nine").textContent
-let zeroButton = document.getElementById("zero").textContent
+let oneButton = document.getElementById("one")
+let twoButton = document.getElementById("two")
+let threeButton = document.getElementById("three")
+let fourButton = document.getElementById("four")
+let fiveButton = document.getElementById("five")
+let sixButton = document.getElementById("six")
+let sevenButton = document.getElementById("seven")
+let eightButton = document.getElementById("eight")
+let nineButton = document.getElementById("nine")
+let zeroButton = document.getElementById("zero")
+
 
 let sumButton = document.getElementById("sum")
 let substractButton = document.getElementById("substract")
 let multiplyButton = document.getElementById("multiply")
 let dividebutton = document.getElementById("divide")
 
-console.log(sumButton.textContent)
+sumButton.addEventListener("click", updateOutput)
+substractButton.addEventListener("click", updateOutput)
+multiplyButton.addEventListener("click", updateOutput)
+dividebutton.addEventListener("click", updateOutput)
+oneButton.addEventListener("click", updateOutput )
+twoButton.addEventListener("click", updateOutput )
+threeButton.addEventListener("click", updateOutput )
+fourButton.addEventListener("click", updateOutput )
+fiveButton.addEventListener("click", updateOutput )
+sixButton.addEventListener("click", updateOutput )
+sevenButton.addEventListener("click", updateOutput )
+eightButton.addEventListener("click", updateOutput )
+nineButton.addEventListener("click", updateOutput )
+zeroButton.addEventListener("click", updateOutput )
 
-sumButton.addEventListener("click", add)
-substractButton.addEventListener("click", substract)
-multiplyButton.addEventListener("click", multiply)
-dividebutton.addEventListener("click", divide)
+
+function updateOutput(output) {
+    console.log(output.target.value)
+    outputResult.textContent += output.target.value
+}
+
+
 
 function operate(currentNumber, newNumber, operator) {
     if (operator === sumButton.textContent) {
