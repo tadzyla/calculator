1c9f83ec626b6333f3ed050ee0952597ab49a0d8
tadzyla <tadas.zvinklys@gmail.com>
2022-12-08 19:42:16 +0000

    adding toggle logic, refactor code

--- index.js
+++ index.js
@@ -1,37 +1,14 @@
-
-let outputResult = document.getElementById("output")
-let oneButton = document.getElementById("one")
-let twoButton = document.getElementById("two")
-let threeButton = document.getElementById("three")
-let fourButton = document.getElementById("four")
-let fiveButton = document.getElementById("five")
-let sixButton = document.getElementById("six")
-let sevenButton = document.getElementById("seven")
-let eightButton = document.getElementById("eight")
-let nineButton = document.getElementById("nine")
-let zeroButton = document.getElementById("zero")
+const mainButtons = document.querySelectorAll(".main_button")
 let totalButton = document.getElementById("total")
+let outputResult = document.querySelector(".output")
 let clearButton = document.getElementById("clear")
+let toggleButton = document.querySelector(".toggle")
+let buttons = document.querySelectorAll("button")
+let title = document.getElementsByTagName("h1")
 
-let sumButton = document.getElementById("sum")
-let substractButton = document.getElementById("substract")
-let multiplyButton = document.getElementById("multiply")
-let dividebutton = document.getElementById("divide")
-
-sumButton.addEventListener("click", updateOutput)
-substractButton.addEventListener("click", updateOutput)
-multiplyButton.addEventListener("click", updateOutput)
-dividebutton.addEventListener("click", updateOutput)
-oneButton.addEventListener("click", updateOutput )
-twoButton.addEventListener("click", updateOutput )
-threeButton.addEventListener("click", updateOutput )
-fourButton.addEventListener("click", updateOutput )
-fiveButton.addEventListener("click", updateOutput )
-sixButton.addEventListener("click", updateOutput )
-sevenButton.addEventListener("click", updateOutput )
-eightButton.addEventListener("click", updateOutput )
-nineButton.addEventListener("click", updateOutput )
-zeroButton.addEventListener("click", updateOutput )
+mainButtons.forEach(button => {
+  button.addEventListener("click", updateOutput)
+})
 
 totalButton.addEventListener("click", () => {
     const lastInput = outputResult.textContent[outputResult.textContent.length - 1];
@@ -45,21 +22,21 @@ totalButton.addEventListener("click", () => {
     }
     outputResult.textContent = eval(outputResult.textContent)
 })
+
 clearButton.addEventListener("click", () => {
     outputResult.textContent = ''
 })
-
 
 function updateOutput(output) {
 
     const lastInput =
       outputResult.textContent[outputResult.textContent.length - 1];
     if (
-        (output.target.value === "+" ||
-        output.target.value === "-" ||
-        output.target.value === "*" ||
-        output.target.value === "/" ) &&(  
-      lastInput === "+" ||
+      (output.target.value === "+" ||
+      output.target.value === "-" ||
+      output.target.value === "*" ||
+      output.target.value === "/") && 
+      (lastInput === "+" ||
       lastInput === "-" ||
       lastInput === "*" ||
       lastInput === "/")
@@ -67,6 +44,22 @@ function updateOutput(output) {
       return; 
     } else {
         outputResult.textContent += output.target.value
-        console.log(outputResult.textContent)
     }
 }
+
+toggleButton.addEventListener("click", toggleMode )
+
+function toggleMode() {
+  document.body.classList.toggle("dark_mode")
+  title[0].classList.toggle("dark_text")
+  outputResult.classList.toggle("output_dark")
+
+  buttons.forEach(button => {
+    button.classList.toggle("dark_mode")
+  })
+
+  toggleButton.children[0].src.includes("/icons/moon.png") ? 
+    toggleButton.children[0].src = "/icons/sun.png" : 
+    toggleButton.children[0].src = "/icons/moon.png" 
+}
+ 
